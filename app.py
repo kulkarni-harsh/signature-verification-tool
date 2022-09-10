22a997f55f75720660de64e86e0313db414d3e11
Harsh Kulkarni <harsh.kulkarni.42774@gmail.com>
2022-09-10 13:22:21 +0530

    streamlit app initial

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+headerSite=st.container()
+introduction=st.container()
+implementation=st.container()
+dataset=st.container()
+testing=st.container()
+credits=st.container()
+
+st.title('Signature Verification Tool')
